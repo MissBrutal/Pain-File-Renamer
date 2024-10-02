877a3f64c0ba397b5a06d2f448bea50472b994d2
Geeky <157978295+Geekyowner@users.noreply.github.com>
2024-10-02 21:35:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 import logging
 import logging.config
 import warnings
-from pyrogram import Client, idle
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
 from config import Config
 from aiohttp import web
 from pytz import timezone
